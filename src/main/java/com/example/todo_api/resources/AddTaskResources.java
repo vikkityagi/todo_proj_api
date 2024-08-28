dc8e0df29ea259fa793f84d1b270e37e27d949b5
Vikki Tyagi <vikkityagi1998@gmail.com>
2024-08-28 12:47:55 +0530

    done

--- src/main/java/com/example/todo_api/resources/AddTaskResources.java
+++ src/main/java/com/example/todo_api/resources/AddTaskResources.java
@@ -5,9 +5,13 @@ import java.time.LocalTime;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AddTaskResources {
 
     @JsonProperty("id")
