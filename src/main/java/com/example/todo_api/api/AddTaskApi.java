4f8ed5b046cba81444ad78103552a019ccd8c753
Vikki Tyagi <vikkityagi1998@gmail.com>
2024-08-28 22:38:57 +0530

    successfully angular proj

--- src/main/java/com/example/todo_api/api/AddTaskApi.java
+++ src/main/java/com/example/todo_api/api/AddTaskApi.java
@@ -7,11 +7,13 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.todo_api.resources.AddTaskResources;
 
 
 @CrossOrigin
+@RequestMapping("/api/v1")
 public interface AddTaskApi {
 
     @PostMapping(value = "/add_task")
--- src/main/resources/static/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-TN7Y544Z.mjs";import"./chunk-FPBEDDSN.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
